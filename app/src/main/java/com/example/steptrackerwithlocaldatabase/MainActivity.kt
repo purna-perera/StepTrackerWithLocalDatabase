e795a3069a378966933aa24a8eb83f7c0f2a6bfd
Purna Perera <purnanperera@gmail.com>
2025-07-09 00:11:52 +1000

    Started developing UI

--- app/src/main/java/com/example/steptrackerwithlocaldatabase/MainActivity.kt
+++ app/src/main/java/com/example/steptrackerwithlocaldatabase/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.steptrackerwithlocaldatabase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.steptrackerwithlocaldatabase.ui.theme.StepTrackerWithLocalDatabaseTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomepageView()
                 }
             }
         }
@@ -30,17 +44,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomepageView() {
+    val myViewModel = HomepageViewModel()
+    Box(Modifier.fillMaxSize()) {
+        Column(Modifier.align(Alignment.Center), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Total Steps", style = MaterialTheme.typography.headlineLarge)
+            Spacer(Modifier.height(8.dp))
+            Text(myViewModel.steps.toString(), style = MaterialTheme.typography.displayMedium)
+            Spacer(Modifier.height(16.dp))
+            Button({
+                myViewModel.steps = 0
+            }) {
+                Text("Reset", style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StepTrackerWithLocalDatabaseTheme {
-        Greeting("Android")
-    }
+class HomepageViewModel() : ViewModel() {
+    var steps by mutableStateOf(0)
 }
